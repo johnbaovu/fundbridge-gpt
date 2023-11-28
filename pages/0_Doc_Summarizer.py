bfbd0f5d4749bd51bafe58a857e12ce18727e3df
johnbaovu <johnbaovu@users.noreply.github.com>
2023-11-28 17:04:19 +0000

    test-nov-29-05

--- pages/0_Doc_Summarizer.py
+++ pages/0_Doc_Summarizer.py
@@ -48,8 +48,8 @@ class DocSummarizer:
                     transcript = loader.load()
 
                 total_token_count = 0
-                for page in transcript:
-                    token_count = num_tokens_from_string(page.page_content,encoding_name='cl100k_base')
+                for i in range(len(transcript)):
+                    token_count = num_tokens_from_string(transcript[i].page_content,encoding_name='cl100k_base')
                     total_token_count += token_count 
                 st.write (f"This document contains {token_count} TOKENS!")
 
